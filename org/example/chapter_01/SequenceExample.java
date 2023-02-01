8ee08fc592c14834abe9a5958b62aef9b4b59457
Edward <edward.aijun.qin@kingland.com>
2023-02-01 17:10:35 +0800

    Add chapter 1 examples

--- org/example/chapter_01/SequenceExample.java
+++ org/example/chapter_01/SequenceExample.java
@@ -6,44 +6,57 @@ import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.stream.Collectors;
 
-class UnsafeSequence {
+interface Sequence {
+    int getNext();
+}
+
+class UnsafeSequence implements Sequence {
     private int value;
 
+    @Override
     public int getNext() {
         return value++;
     }
 }
 
-class UnsafeSequenceTest {
+class SafeSequence implements Sequence {
+    private int value;
+
+    @Override
+    public synchronized int getNext() {
+        return value++;
+    }
+}
+
+
+class SequenceExample {
 
     public static void main(String[] args) throws Exception {
-        UnsafeSequence unsafeSequence = new UnsafeSequence();
+        System.out.println("Test UnsafeSequence - Thread Safe: " + isSafeSequence(new UnsafeSequence()));
+        System.out.println("Test SafeSequence - Thread Safe: " + isSafeSequence(new SafeSequence()));
+    }
+
+    public static boolean isSafeSequence(Sequence sequence) throws Exception {
         List<Integer> seqList1 = Collections.synchronizedList(new ArrayList<>());
         List<Integer> seqList2 = Collections.synchronizedList(new ArrayList<>());
         ExecutorService executorService = Executors.newFixedThreadPool(2);
         CountDownLatch countDownLatch = new CountDownLatch(2);
         executorService.submit(() -> {
             for (int i = 0; i < 100000; i++) {
-                seqList1.add(unsafeSequence.getNext());
+                seqList1.add(sequence.getNext());
             }
             countDownLatch.countDown();
         });
         executorService.submit(() -> {
             for (int i = 0; i < 100000; i++) {
-                seqList2.add(unsafeSequence.getNext());
+                seqList2.add(sequence.getNext());
             }
             countDownLatch.countDown();
         });
         countDownLatch.await();
         executorService.shutdown();
-        List<Integer> sameSeqList = seqList1.stream().filter(s1 -> seqList2.stream().anyMatch(s1::equals)).collect(Collectors.toList());
-        if (!sameSeqList.isEmpty()) {
-            System.out.println("Found the same value!");
-        }
+
+        return seqList1.stream().noneMatch(s1 -> seqList2.stream().anyMatch(s1::equals));
     }
 }
-
-
-
