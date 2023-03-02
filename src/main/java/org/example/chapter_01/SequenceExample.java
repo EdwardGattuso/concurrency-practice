b1b497f71061754851a5e416d69b4ccf64941be6
Edward <edward.aijun.qin@kingland.com>
2023-03-02 08:41:19 +0800

    Add chapter 5 examples

--- src/main/java/org/example/chapter_01/SequenceExample.java
+++ src/main/java/org/example/chapter_01/SequenceExample.java
@@ -1,11 +1,14 @@
 package org.example.chapter_01;
 
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.stream.IntStream;
 
 interface Sequence {
     int getNext();
@@ -32,31 +35,24 @@ class SafeSequence implements Sequence {
 
 class SequenceExample {
 
+    private static final int THREAD_COUNT = 10;
+
     public static void main(String[] args) throws Exception {
         System.out.println("Test UnsafeSequence - Thread Safe: " + isSafeSequence(new UnsafeSequence()));
         System.out.println("Test SafeSequence - Thread Safe: " + isSafeSequence(new SafeSequence()));
     }
 
-    public static boolean isSafeSequence(Sequence sequence) throws Exception {
-        List<Integer> seqList1 = Collections.synchronizedList(new ArrayList<>());
-        List<Integer> seqList2 = Collections.synchronizedList(new ArrayList<>());
-        ExecutorService executorService = Executors.newFixedThreadPool(2);
-        CountDownLatch countDownLatch = new CountDownLatch(2);
-        executorService.submit(() -> {
-            for (int i = 0; i < 100000; i++) {
-                seqList1.add(sequence.getNext());
-            }
+    public static boolean isSafeSequence(Sequence sequence) throws InterruptedException {
+        List<Integer> seqList = Collections.synchronizedList(new ArrayList<>());
+        ExecutorService executorService = Executors.newFixedThreadPool(THREAD_COUNT);
+        CountDownLatch countDownLatch = new CountDownLatch(THREAD_COUNT);
+        IntStream.range(0, THREAD_COUNT).forEach(t -> executorService.submit(() -> {
+            IntStream.range(0, 10000).forEach(i -> seqList.add(sequence.getNext()));
             countDownLatch.countDown();
-        });
-        executorService.submit(() -> {
-            for (int i = 0; i < 100000; i++) {
-                seqList2.add(sequence.getNext());
-            }
-            countDownLatch.countDown();
-        });
+        }));
         countDownLatch.await();
         executorService.shutdown();
 
-        return seqList1.stream().noneMatch(s1 -> seqList2.stream().anyMatch(s1::equals));
+        return new HashSet<>(seqList).size() == 10000 * THREAD_COUNT;
     }
 }
